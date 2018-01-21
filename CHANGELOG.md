d8b14d89bf99128751ee48148eb2f09a72e6c3ed
Xi ErDe <edxi@msn.com>
2018-01-21 21:02:49 +0800

    Dev (#1)
    Create Scaffold

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [vNext] Unreleased
+
+### Fixed
+
+- test file
+
+## [0.0.1] 2018-01-20
+
+initial release
---
+++ VMwareBaselineCheck/Public/Compare-ScriptOutput.ps1
@@ -0,0 +1,68 @@
+<#
+    .SYNOPSIS
+    Load a pre-defined .csv file to check baseline item with Script outputs.
+
+    .DESCRIPTION
+    Read input from .csv file
+    Run script block which defined in .csv file, to generate outputs.
+    Compare items in .csv file with script outputs.
+    Output compare results to report csv file with hostname-datetime.
+
+    .PARAMETER csvfile
+    Input a .csv file which pre-defined format.
+
+    .EXAMPLE
+    Compare-ScriptOutput baselineGP.csv
+
+    .OUTPUTS
+    The compare results to report .csv file with hostname-datetime.
+
+    .NOTES
+    Author: Xi ErDe
+    Date:   Jan 14, 2018
+#>
+
+function Compare-ScriptOutput {
+    param (
+        [CmdletBinding(SupportsShouldProcess = $true, DefaultParameterSetName = 'csvfile')]
+        [Parameter(
+            Position = 0,
+            ParameterSetName = 'csvfile',
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Path to one or more csv file.')]
+        [ValidateNotNullOrEmpty()]
+        [SupportsWildcards()]
+        [string[]]
+        $csvfile = ''
+    )
+
+    begin {
+        while ($csvfile[0] -eq '') {$csvfile = Get-FileName}
+        $allBaselineSettings = @()
+    }
+
+    process {
+        # Import baseline items from .csv file
+        foreach ($acsvfile in $csvfile) {
+            $allBaselineSettings += Import-Csv -Path $acsvfile
+        }
+
+        if (($allBaselineSettings |Get-Member -MemberType 'NoteProperty' | Select-Object -ExpandProperty 'Name') -notcontains 'Actual Value') {
+            $allBaselineSettings | Add-Member -Name "Actual Value" -MemberType NoteProperty
+        }
+        if (($allBaselineSettings |Get-Member -MemberType 'NoteProperty' | Select-Object -ExpandProperty 'Name') -notcontains 'Check Result') {
+            $allBaselineSettings | Add-Member -Name "Check Result" -MemberType NoteProperty
+        }
+
+        $allBaselineSettings | Where-Object {$_.Script -ne '' -and $_.'Baseline Value' -ne ''} | ForEach-Object {
+            $ScriptReturn = &([Scriptblock]::Create($_.Script))
+            $_.'Actual Value' = $ScriptReturn['Actual Value']
+            $_.'Check Result' = $ScriptReturn['Check Result']
+        }
+    }
+
+    end {
+        $allBaselineSettings | Export-Csv -Path "$env:TEMP\$env:COMPUTERNAME-$(Get-Date -UFormat "%Y%m%d-%H%M%S").csv" -NoTypeInformation
+    }
+}
